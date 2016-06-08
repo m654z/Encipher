af9e02579b2732522be0b649ada352d392a0f28c
William Muhu <wmuhucraft@gmail.com>
2016-06-08 10:32:34 +0300

    Removed useless code

--- encipher.py
+++ encipher.py
@@ -5,24 +5,26 @@ def encode(text):
     digits = list(string.digits)
     text = list(text)
     pword = ""
+    
+    # Get a random sequence of integers from 0-9
     for i in range(0, len(text)):
         pword += str(random.choice(digits))
         
     key = list(str(pword))
-        
+    
     encoded = ""
     keyI = 0
+
+    # Add the random sequence to the ASCII values of the string
     for c in text:
         encoded += chr(ord(c) + int(key[keyI]))
         keyI += 1
 
     return [encoded, ''.join(key)]
 
-def decode(text, key):       
+def decode(text, key):
     text = list(text)
-    pword = ""
-    for i in range(0, len(key)):
-        pword += str(key[i])
+    pword = str(key)
 
     key = list(str(pword))
         
